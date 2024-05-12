38ba1b1e183a60907c73085f62afc368f776be3b
seeleng <seelengxd@gmail.com>
2024-05-12 11:58:48 +0800

    fix: fix deployment host issue
    nextjs was complaining on vps that localhost:3000 does not match my
    domain name
    
    https://raddy.dev/blog/next-js-14-0-2-x-forwarded-host-header-with-value-localhost3000-does-not-match-origin-fix/

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["chatbot.seeleng.dev", "localhost:3000"],
+    },
+  },
+};
 
 export default nextConfig;
