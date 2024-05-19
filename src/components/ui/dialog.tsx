1ebc70afdc298c12254b3570ca39cdaf24748605
Lu Yiting <e0774189@u.nus.edu>
2024-05-19 17:02:02 +0800

    Improve dashboard UI.

---
+++ src/components/ui/dialog.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { Cross2Icon } from '@radix-ui/react-icons';
+
+const DialogTemplate = ({ buttonContent, title, description }) => (
+  <>
+  <Dialog.Root>
+    <Dialog.Trigger asChild>
+      <button className="m-2 text-red-500 hover:bg-red-200 inline-flex h-[20px] w-[20px] items-center justify-center rounded-[10px] bg-red-300 px-[15px] font-medium leading-none shadow-[0_2px_10px]">
+        {buttonContent}
+      </button>
+    </Dialog.Trigger>
+    <Dialog.Portal>
+      <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
+      <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+        <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
+          {title}
+        </Dialog.Title>
+        <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
+          {description}
+        </Dialog.Description>
+        <div className="mt-[25px] flex justify-end">
+          <Dialog.Close asChild>
+            <button className="bg-red-300 text-white hover:bg-red-500 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none">
+              Close
+            </button>
+          </Dialog.Close>
+        </div>
+        <Dialog.Close asChild>
+          <button
+            className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+            aria-label="Close"
+          >
+            <Cross2Icon></Cross2Icon>
+          </button>
+        </Dialog.Close>
+      </Dialog.Content>
+    </Dialog.Portal>
+  </Dialog.Root>
+  </>
+);
+
+export default DialogTemplate;
