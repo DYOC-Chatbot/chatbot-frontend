3e2430fa21a1ba8f5cc2c7a9e745b456cae9c86c
Luo <e0969568@u.nus.edu>
2024-05-30 00:23:08 +0800

    Create homepage

---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,10 @@
+export default function Homepage() {
+  return (
+    <main className="flex justify-center">
+      <div className="m-8 sm:w-96 shadow-md flex flex-col align-center space-y-2 p-8">
+        <h1 className="font-bold text-2xl pb-4">Homepage</h1>
+        <p>Welcome to the homepage!</p>
+      </div>
+    </main>
+  );
+}
