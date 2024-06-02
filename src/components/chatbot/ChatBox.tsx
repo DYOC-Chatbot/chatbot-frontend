22f2fed839c91269025eb516b5bb1ebd39dc348d
tankh99 <khanghou99@gmail.com>
2024-06-02 20:08:16 +0800

    feat: setup receiving messages

--- src/components/chatbot/ChatBox.tsx
+++ src/components/chatbot/ChatBox.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ChatBoxForm from './ChatBoxForm';
 import ChatMessage from './ChatMessage';
 import { useChat } from '@/hooks/useChat';
 import { ByUser, Message } from '@/types/messages/message';
+import { getCookie } from 'cookies-next';
 
 type P = {
   // ID of the chat to retrieve history of messages from. Not the TG Chat ID, but the actual chat Id stored in database
@@ -13,6 +14,35 @@ export default function ChatBox({chatId}: P) {
   const {data: chat, isPending} = useChat(chatId);
 
   const [messages, setMessages] = useState<Message[]>(chat?.messages || [])
+
+  useEffect(() => {
+    try {
+      const socket = new WebSocket('ws://localhost:8080/api/ws');
+  
+  
+      socket.onopen = () => {
+        console.log('WebSocket connection established');
+      };
+  
+      socket.onmessage = (event) => {
+        console.log('Message from server: ', event.data);
+      };
+  
+      socket.onclose = () => {
+        console.log('WebSocket connection closed');
+      };
+  
+      socket.onerror = (ev) => {
+        console.error('error event: ', ev);
+      };
+      return () => {
+        socket.close();
+      };
+    } catch (error) {
+      console.log(error)
+    }
+
+  }, [])
 
   const onSubmit = async (values: any) => {
     setMessages([...messages, {
