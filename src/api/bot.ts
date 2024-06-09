d300293b9851a014c6a9a3beda9c031e6b24a95a
tankh99 <56617918+tankh99@users.noreply.github.com>
2024-06-09 14:50:07 +0800

    Update src/api/bot.ts
    Co-authored-by: see leng <34371483+seelengxd@users.noreply.github.com>

--- src/api/bot.ts
+++ src/api/bot.ts
@@ -2,7 +2,7 @@ import { API, BaseAPI } from "./base";
 
 export class BotAPI extends BaseAPI {
     static async sendMessage(chatId: number, message: string) {
-        // This only retursn the success message, so we don't care about the return result
+        // This only returns the success message, so we don't care about the return result
         const res = await super.post(`${API}/bot/send/${chatId}`, {message})
         return;
     }
